9662e16de46f09b056b050aee6af023e8257f56d
耿朝继 <36151545+gengzhaoji@users.noreply.github.com>
2021-06-15 16:36:53 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const Timestamp = new Date().getTime()
+
 const path = require('path')
 
 // 导入compression-webpack-plugin
@@ -64,7 +66,11 @@ module.exports = {
     }
   },
   configureWebpack: {
-    name: 'vue3项目'
+    name: 'vue3项目',
+    output: { // 输出重构  打包编译后的 文件名称
+      filename: `js/[name].[chunkhash].${Timestamp}.js`,
+      chunkFilename: `js/[name].[chunkhash].${Timestamp}.js`
+    }
   },
   // webpack-dev-server 相关配置
   devServer: {
